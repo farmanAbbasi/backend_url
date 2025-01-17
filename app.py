1dc8be33d687a49f5c3fc4f86477d1afbf5ff6b6
Mohammad Farman Abbasi <32016615+farmanAbbasi@users.noreply.github.com>
2025-01-17 16:37:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 import requests
 from flask import Flask
 from flask import request
@@ -8,16 +7,16 @@ import os
 from flask_cors import CORS, cross_origin
 CORS(app)
 
-def getMovieUrl(movieName):
-    url="http://putlockerstv.online/rogue-2020/"
-    url = "http://123movies4u.site/"+movieName+"/"
-    html_content = requests.get(url).text
-    soup = BeautifulSoup(html_content, "html.parser")
-    try:
-        movieUrl=soup.find_all('iframe')[0]['src']
-    except Exception as e:
-        movieUrl=""
-    return movieUrl
+# def getMovieUrl(movieName):
+#     url="http://putlockerstv.online/rogue-2020/"
+#     url = "http://123movies4u.site/"+movieName+"/"
+#     html_content = requests.get(url).text
+#     soup = BeautifulSoup(html_content, "html.parser")
+#     try:
+#         movieUrl=soup.find_all('iframe')[0]['src']
+#     except Exception as e:
+#         movieUrl=""
+#     return movieUrl
 
 def getCorsEnabled(url,postData,requestMethod):
     if requestMethod == 'GET':
@@ -40,18 +39,18 @@ def cors():
         return json.dumps(content)    
         
 
-@app.route('/loadData', methods=['GET'])
-def loadData():
-    names=[]
-    names.append(request.args.get('name1'))
-    names.append(request.args.get('name2'))
+# @app.route('/loadData', methods=['GET'])
+# def loadData():
+#     names=[]
+#     names.append(request.args.get('name1'))
+#     names.append(request.args.get('name2'))
     
-    for name in names:
-        finalName=getMovieUrl(name)
-        #if got the url 
-        if(finalName!=""):
-            break
-    return json.dumps({"url": finalName})    
+#     for name in names:
+#         finalName=getMovieUrl(name)
+#         #if got the url 
+#         if(finalName!=""):
+#             break
+#     return json.dumps({"url": finalName})    
 
 @app.route('/', methods=['GET'])
 def getData():
